8297e5e52c5ed4e04ffa4639d9ec2fc0b28f629e
Veronica <veronica.stevania@gmail.com>
2022-12-28 23:10:58 +0100

    Fix css page Lodging + display error404 when the page is not found

--- src/pages/Lodging/Lodging.jsx
+++ src/pages/Lodging/Lodging.jsx
@@ -9,18 +9,19 @@ import "../../pages/Lodging/Lodging.css"
 import LodgingDescription from "../../components/LodgingDescription/LodgingDescription"
 import LodgingEquipment from "../../components/LodgingEquipment/LodgingEquipment"
 import Footer from "../../components/Footer/Footer"
+import Error404 from "../Error404/Error404"
 
 const Lodging = () => {    
     useEffect(() => {
-        window.scrollTo(0,0)                                            /* On load, the page is displayed at its top */  
+        window.scrollTo(0,0)                                         /* On load, the page is displayed at its top */  
     })
 
-    const {id} = useParams()            /* Retrieve username from URL */
-    const location = useLocation()      /* This hook returns the current location object */ 
+    const {id} = useParams()                                        /* Retrieve username from URL */
+    const location = useLocation()                                  /* This hook returns the current location object */ 
     const housing = location.state
-    console.log (housing)    
+    console.log ("housing : " + housing)    
 
-    return (
+    if(housing != null ) { return (
         <div className='lodging'>
             <Header />
             <div className='main-lodging'>
@@ -39,7 +40,9 @@ const Lodging = () => {
             </div>  
             <Footer />   
         </div>        
-    )
+    )} else {
+    return (<Error404 />)
+    }
 }
 
 export default Lodging
